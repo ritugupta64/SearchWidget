adb0bcaad6b811e52e1fc453decde620acd3936e
ritugupta64 <gritu64@gmail.com>
2019-10-12 21:36:59 +0530

    Updates

---
+++ precache-manifest.bc1ff0e49fe5e5de19ec5919e1a006c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e704932e030e5e0b0ecdc451515522",
+    "url": "/SearchWidget/index.html"
+  },
+  {
+    "revision": "6d76de8f3e1282e32424",
+    "url": "/SearchWidget/static/js/2.793cd403.chunk.js"
+  },
+  {
+    "revision": "7f710f52252171621f8a",
+    "url": "/SearchWidget/static/js/main.1527ee4d.chunk.js"
+  },
+  {
+    "revision": "12aa4ec93d5580ee271a",
+    "url": "/SearchWidget/static/js/runtime-main.01004281.js"
+  }
+]);
